52f16e8434be19185071e25bc99a617dde85f8c1
Mike VanSleen <mike.vansleen@gmail.com>
2019-07-26 09:15:54 -0600

    Initial commit, installed deps, set up index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+
+import "./index.scss";
+import "semantic-ui-css/semantic.min.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(() => {}, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
